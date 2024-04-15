9925dad9bd39adb010618e6f5049f9648df23332
pau-cardoso <paulinacardoso7@gmail.com>
2024-04-14 22:20:25 -0600

    refactor: crop images

---
+++ M2/helper.py
@@ -0,0 +1,92 @@
+import cv2
+import os
+import numpy as np
+
+
+def resize_images():
+    # Tamaños deseados
+    target_width = 791
+    target_height = 548
+
+    # Directorio con las imagenes
+    input_dir = "./ZeldaLevels/train/playable"
+    output_dir = "resizing/train/playable"
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Lee la imagen
+            image_path = os.path.join(input_dir, filename)
+            image = cv2.imread(image_path)
+
+            # Cambia el tamaño de la imagen
+            resized_image = cv2.resize(image, (target_width, target_height))
+
+            # Guarda la imagen con el nuevo tamaño
+            output_path = os.path.join(output_dir, filename)
+            cv2.imwrite(output_path, resized_image)
+
+    print("All images resized and saved successfully!")
+
+
+def normalize_images():
+    # Directorio que contiene tus imágenes originales
+    input_dir = "./ZeldaLevels/test/unplayable"
+    output_dir = "normalize/test/unplayable"
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Loop a través de cada imagen en el directorio de entrada
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Lee la imagen
+            image_path = os.path.join(input_dir, filename)
+            image = cv2.imread(image_path)
+
+            # Normaliza la imagen dividiendo por 255
+            normalized_image = image / 255.0
+
+            # Guarda la imagen normalizada en el directorio de salida
+            output_path = os.path.join(output_dir, filename)
+            cv2.imwrite(output_path, (normalized_image * 255).astype(np.uint8))  # vuelve a escalar a 0-255 antes de guardarla
+
+    print("Todas las imágenes han sido normalizadas y guardadas exitosamente!")
+
+
+def crop_images():
+    new_width = 675
+    new_height = 433
+
+    # Directorio que contiene las imágenes originales
+    input_dir = "./ZeldaLevels/train/unplayable"
+    output_dir = "crop/train/unplayable"
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Lee la imagen
+            image_path = os.path.join(input_dir, filename)
+            image = cv2.imread(image_path)
+
+            # Obtener las dimensiones de la imagen
+            height, width = image.shape[:2]
+
+            # Calcular las coordenadas del rectángulo de recorte centrado
+            start_x = max(0, int((width - new_width) / 2))
+            start_y = max(0, int((height - new_height) / 2))
+            end_x = min(width, start_x + new_width)
+            end_y = min(height, start_y + new_height)
+
+            # Recortar la región de interés centrada
+            cropped_image = image[start_y:end_y, start_x:end_x]
+
+            # Write the cropped image to the output directory
+            output_path = os.path.join(output_dir, filename)
+            cv2.imwrite(output_path, cropped_image)
+
+
+
+# Escribir aquí las transformaciones que se deseen correr
+# crop_images()
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# aplicaciones-avanzadas
+# Aplicaciones Avanzadas
+En este repositorio se encuentran los archivos de la materia **TC3002B: Desarrollo de aplicaciones avanzadas de ciencias computacionales**
+
+En el fólder `M2/ZeldaLevels` se estará realizando un algoritmo de ML que tomará un dataset en donde están diferentes imágenes con diferentes niveles del videojuego Zelda. La máquina deberá aprender a reconocer si el nivel de la imagen es jugable o no jugable. La manera de reconocer si un nivel es jugable es porque se debe cumplir que:
+1. El personaje de Link se encuentre en el nivel
+2. Haya al menos una puerta a la que Link pueda avanzar
+3. Haya al menos una llave para que Link pueda acceder a la puerta
+
+De no cumplirse alguno de estos puntos, el nivel sería clasificado como no jugable.
+
+Las imágenes que se estarán ocupando se encuentran ya divididas en fólders para entrenar y testear el algoritmo. Dentro de estos fólders, se vuelven a dividir en los niveles jugables y no jugables (playable y unplayable). Asimismo, ya en los fólders todo el dataset pasó por técnicas de escalamiento y preprocesado como lo fue:
+- Eliminacion de ruido
+- El redimensionar todas las imágenes para que estén con la misma dimensión ya que variaba en cada una.
+- Recortar las imagenes para que solo tome en cuenta la parte que nos interesa, remover el marco de la imagen que no afectaba a la decisión
+- Normalizar las imágenes
+
+Todo esto se realizó con la ayuda de unos scripts que se encuentran en el archivo `helper.py`, mismo que se puede volver a ocupar en caso de requerirlo en el algoritmo.
+
+
+
+
+El dataset original fue recuperado de: https://www.kaggle.com/datasets/adizafar/zelda-game-levels/data
